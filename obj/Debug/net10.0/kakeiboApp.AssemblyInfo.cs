86430f5f1d1a1aeb4c3d15154142a6c4a054be04
Amon-Shibata <amon.shibata@ge-creative.co.jp>
2026-04-17 11:23:14 +0900

    Merge main into shibata0417

--- obj/Debug/net10.0/kakeiboApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/kakeiboApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kakeiboApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53bc83a220e2009281089f0c7e7e3ecc07bd257f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2108394815939c47a3f1a2eb5d515429c8bc41")]
 [assembly: System.Reflection.AssemblyProductAttribute("kakeiboApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kakeiboApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
