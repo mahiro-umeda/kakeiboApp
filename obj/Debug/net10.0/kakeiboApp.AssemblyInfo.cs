9adac8d05681be09f2891fb49ef12f66ddd95377
Semba1008 <yuki.semba@ge-creative.co.jp>
2026-04-20 15:53:16 +0900

    変更内容

--- obj/Debug/net10.0/kakeiboApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/kakeiboApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kakeiboApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8732118a3fd7896975dbb59e10dcb973b4bc3191")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394aeb12cb9cf75039cbb00c1cad257ef31ec000")]
 [assembly: System.Reflection.AssemblyProductAttribute("kakeiboApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kakeiboApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
